ed1d27c4a02e8c7a79b5f809682d8e16bb1ac7bc
vikab <vikabashlykova2007@gmail.com>
2026-02-25 14:00:27 +0300

    начало: минимальный дизайн и навигация

---
+++ PiTPM_Bashlykova523/Pages/PageF2.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PiTPM_Bashlykova523.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageF2.xaml
+    /// </summary>
+    public partial class PageF2 : Page
+    {
+        public PageF2()
+        {
+            InitializeComponent();
+        }
+
+        private void backBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+
+        private void nextFuncBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new PageF3());
+        }
+    }
+}
---
+++ PiTPM_Bashlykova523/Pages/PageF3.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PiTPM_Bashlykova523.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageF3.xaml
+    /// </summary>
+    public partial class PageF3 : Page
+    {
+        public PageF3()
+        {
+            InitializeComponent();
+        }
+
+        private void backBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+    }
+}
