515f953e5857fc53e3be277d9f435855b6bf2f55
vikab <vikabashlykova2007@gmail.com>
2026-03-19 13:23:11 +0300

    создан тестовый проект и черновой тест

---
+++ FuncTests/FuncUnitTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace FuncTests
+{
+    [TestClass]
+    public class FuncUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int res = 2 + 2;
+            Assert.AreEqual(res, 4);
+            Assert.AreNotEqual(res, 5);
+            Assert.IsFalse(res > 5);
+            Assert.IsTrue(res < 5);
+        }
+    }
+}
---
+++ FuncTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FuncTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FuncTests")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("987fa98a-6e81-4a6a-bd4d-e99f02bf692f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
