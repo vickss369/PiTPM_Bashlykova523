c4ff30e171bd97b3ded9f87ddd6c34d3fb000966
vikab <vikabashlykova2007@gmail.com>
2026-03-20 16:28:53 +0300

    тесты функции 3: вычисление дробных значений, сообщение о делении на ноль, проверка границ

--- FuncTests/FuncUnitTest.cs
+++ FuncTests/FuncUnitTest.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using PiTPM_Bashlykova523.Pages;
+using System.Linq;
 
 namespace FuncTests
 {
@@ -17,8 +18,8 @@ namespace FuncTests
             Assert.IsTrue(res < 5);
         }*/
 
-        /*[TestMethod]
-        public void TestFunc1_Value1() 
+        [TestMethod]
+        public void TestFunc1_Value1()
         {
             var pF1 = new PageF1();
 
@@ -42,23 +43,23 @@ namespace FuncTests
         [TestMethod]
         public void TestFunc1_InvalidZ_ShouldShowMessage()
         {
-            var page = new PageF1();
+            var pF1 = new PageF1();
 
-            bool res = page.CalculateF1(1, 1, 2, out double result, out string error);
+            bool ans = pF1.CalculateF1(1, 1, 2, out double answer, out string error);
 
-            Assert.IsFalse(res);
+            Assert.IsFalse(ans);
             Assert.AreEqual("Арксинус определён только для значений от -1 до 1!", error);
-        }*/
+        }
 
         [TestMethod]
         public void TestFunc2_Sh_SqrtBranch_CorrectAns()
         {
-            var page = new PageF2();
+            var pF2 = new PageF2();
 
             double x = 0.5;
             double b = 0.5;
 
-            bool ans = page.CalculateF2(x, b, "sh", out double answer, out string error);
+            bool ans = pF2.CalculateF2(x, b, "sh", out double answer, out string error);
             double expected = Math.Sqrt(Math.Abs(Math.Sinh(x) + b));
 
             Assert.IsTrue(ans);
@@ -68,12 +69,12 @@ namespace FuncTests
         [TestMethod]
         public void TestFunc2_2ndBranch_BorderCase_ShouldFail()
         {
-            var page = new PageF2();
+            var pF2 = new PageF2();
 
             double x = 1;
             double b = 0.5;
 
-            bool ans = page.CalculateF2(x, b, "x2", out double answer, out string error);
+            bool ans = pF2.CalculateF2(x, b, "x2", out double answer, out string error);
             double wrongExpected = Math.Sqrt(Math.Abs(Math.Pow(x, 2) + b));
 
             Assert.AreEqual(wrongExpected, answer, 0.0001);
@@ -82,9 +83,9 @@ namespace FuncTests
         [TestMethod]
         public void TestFunc2_InvalidFunction_SouldShowMessage()
         {
-            var page = new PageF2();
+            var pF2 = new PageF2();
 
-            bool ans = page.CalculateF2(1, 1, "бебебе", out double answer, out string error);
+            bool ans = pF2.CalculateF2(1, 1, "бебебе", out double answer, out string error);
 
             Assert.IsFalse(ans);
             StringAssert.Contains(error, "Неизвестная");
@@ -93,13 +94,56 @@ namespace FuncTests
         [TestMethod]
         public void TestFunc2_ShouldFail_ExpectErrorButValid()
         {
-            var page = new PageF2();
+            var pF2 = new PageF2();
 
-            bool ans = page.CalculateF2(1, 1, "x2", out double answer, out string error);
+            bool ans = pF2.CalculateF2(1, 1, "x2", out double answer, out string error);
             if (ans)
             {
                 Assert.Fail("Ожидалась ошибка, но вычисление прошло успешно!");
             }
+        }
+
+        [TestMethod]
+        public void TestFunc3_FractionValues_CorrectResult()
+        {
+            var pF3 = new PageF3();
+
+            bool res = pF3.CalculateF3(0.5, 1.0, 0.25, 0.3, out var result, out string error);
+            Assert.IsTrue(res);
+            Assert.AreEqual(3, result.Count);
+
+            double expectedFirstY = Math.Pow(0.5, 2) + Math.Tan(5 * 0.5 + 0.3 / 0.5);
+            Assert.AreEqual(expectedFirstY, result[0].y, 0.0001);
+        }
+
+        [TestMethod]
+        public void TestFunc3_DivisioByZero_ShouldShowMessage()
+        {
+            var pF3 = new PageF3();
+
+            bool res = pF3.CalculateF3(-1, 1, 0.5, 1, out var result, out string error);
+            if (res)
+            {
+                Assert.IsFalse(result.Any(p => Math.Abs(p.x) < 1e-10),
+                    "Ноль должен был вызвать ошибку, но был пропущен!");
+            }
+            else
+            {
+                StringAssert.Contains(error, "ноль");
+            }
+        }
+
+        [TestMethod]
+        public void TestFunc3_LastPointIncluded_ShouldFail()
+        {
+            var pF3 = new PageF3();
+
+            bool res = pF3.CalculateF3(1, 2, 0.3, 1, out var result, out string error);
+            Assert.IsTrue(res);
+
+            double lastX = result.Last().x;
+            Assert.IsTrue(Math.Abs(lastX - 2) < 0.0001,
+                "Последняя точка интервала не достигнута");
         }
     }
 }
